e50b9b1e9cb954f1123cef2e611dc6df65b5d93c
guig <gg@chaochuang.com.cn>
2018-09-05 17:46:31 +0800

    one build more tag

--- ccibmjava/8/sdk/build.sh
+++ ccibmjava/8/sdk/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-docker build -t chaochuang/ccibmjava:8-sdk .
-docker image tag chaochuang/ccibmjava:8-sdk chaochuang/ccibmjava:latest
+docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava .
