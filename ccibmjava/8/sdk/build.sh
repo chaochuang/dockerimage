7d071c5c144147346d0244a29e8316e28840bdc4
guig <gg@chaochuang.com.cn>
2018-09-05 18:04:39 +0800

    build and push

--- ccibmjava/8/sdk/build.sh
+++ ccibmjava/8/sdk/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava .
+docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava . \
+	&& docker image push chaochuang/ccibmjava:8-sdk \
+	&& docker image push chaochuang/ccibmjava
