ccb9802debaf3c84aa3750fc06cfac977bdadbc1
guig <gg@chaochuang.com.cn>
2018-09-06 09:19:11 +0800

    优化构建脚本处理方式

--- ccibmjava/8/sdk/build.sh
+++ ccibmjava/8/sdk/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava . \
+origdir=`pwd`
+
+rootdir=`dirname $0`
+rootdir=`cd "$rootdir" ; pwd`
+
+cd "$rootdir"
+
+docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava:latest . \
 	&& docker image push chaochuang/ccibmjava:8-sdk \
-	&& docker image push chaochuang/ccibmjava
+	&& docker image push chaochuang/ccibmjava:latest
+
+cd "$origdir"
