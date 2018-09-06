d78a7c96444b6156a362ac5b6d80675bce8f75a2
guig <gg@chaochuang.com.cn>
2018-09-06 09:28:52 +0800

    增加 sdk tag

--- ccibmjava/8/sdk/build.sh
+++ ccibmjava/8/sdk/build.sh
@@ -7,8 +7,9 @@ rootdir=`cd "$rootdir" ; pwd`
 
 cd "$rootdir"
 
-docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava:latest . \
+docker build -t chaochuang/ccibmjava:8-sdk -t chaochuang/ccibmjava:sdk -t chaochuang/ccibmjava:latest . \
 	&& docker image push chaochuang/ccibmjava:8-sdk \
+	&& docker image push chaochuang/ccibmjava:sdk \
 	&& docker image push chaochuang/ccibmjava:latest
 
 cd "$origdir"
